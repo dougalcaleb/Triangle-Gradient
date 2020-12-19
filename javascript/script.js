b12830a06abc5168efc0babf21a7dbf1feb52691
Caleb <dougalcaleb@gmail.com>
2020-12-18 18:53:47 -0700

    multiple palettes, new features

--- javascript/script.js
+++ javascript/script.js
@@ -1,26 +1,61 @@
 const canvas = document.querySelector("canvas");
 let c = canvas.getContext("2d");
+let imageData;
 
+// controls
 let variance = 100;
 let vertCount = [20, 10];
-let verts = [];
-let avgs = [];
+let lineColor = "#111111";
 let canvasSize = [2560, 1440];
-let rotation = 0;
-let colors = ["#eb640a", "#faffad", "#c7ffad", "#98fb6a", "#53c021", "#084200"];
-let tempColors = [];
 let gradAngle = 0;
 let radialGradient = false;
-let gradMode = "calculate"; // snap or calculate
-let lineColor = "#111111";
+let randColor = 0;
+let brightnessBalance = 0;
+let colors = [];
+let palettes = [
+   ["#eb640a", "#faffad", "#c7ffad", "#98fb6a", "#53c021", "#084200"]
+];
+
+let verts = [];
+let avgs = [];
+let tempColors = [];
+let gradMode = "calculate"; // snap or calculate -- doesn't work
+
 
 let outline = false;
 let showVerts = false;
 let showAvgs = false;
 
-let imageData;
+let arrowSuffix = "s";
+let arrowAngles = {
+   s: [0, 90, 180, 270],
+   d: []
+};
+let activePalette = 0;
+let editingPalette = -1;
+
+let defaults = {
+   gradAngle: 0,
+   variance: 100,
+   vertCount: [20, 10],
+   randColor: 0,
+   brightnessBalance: 0,
+};
+
+
 
 let Debug = document.querySelector(".debug");
+
+// to do:
+/* 
+- export
+- radial / linear grad
+- outline control
+- save functionality
+- multiple palettes
+- light/dark balance (similar to randomness but only brightness)
+
+*/
 
 /*
 ===================================================================================
@@ -32,16 +67,20 @@ CONTROLS
 
 document.querySelector(".height").addEventListener("input", function () {
 	canvasSize[1] = this.value;
-	canvas.setAttribute("height", canvasSize[1]);
+   canvas.setAttribute("height", canvasSize[1]);
+   adjustAngle();
 });
 
 document.querySelector(".width").addEventListener("input", function () {
 	canvasSize[0] = this.value;
-	canvas.setAttribute("width", canvasSize[0]);
+   canvas.setAttribute("width", canvasSize[0]);
+   adjustAngle();
 });
 
 document.querySelector(".rotation").addEventListener("input", function () {
-	gradAngle = parseInt(this.value);
+   gradAngle = parseInt(this.value);
+   arrowDir = 0;
+   // clear btns
    createGradient();
    drawBoxes();
 });
@@ -49,10 +88,91 @@ document.querySelector(".rotation").addEventListener("input", function () {
 document.querySelector(".variance").addEventListener("input", function () {
    variance = parseInt(this.value);
    c.clearRect(0, 0, canvasSize[0], canvasSize[1]);
+   verts = [];
+   createGradient();
    generateVertices();
-   // drawBoxes();
 });
 
+document.querySelector(".randomness").addEventListener("input", function () {
+   randColor = parseInt(this.value);
+   c.clearRect(0, 0, canvasSize[0], canvasSize[1]);
+   createGradient();
+   drawBoxes();
+});
+
+document.querySelector(".brightness").addEventListener("input", function () {
+   brightnessBalance = parseInt(this.value) - 50;
+   c.clearRect(0, 0, canvasSize[0], canvasSize[1]);
+   createGradient();
+   drawBoxes();
+});
+
+document.querySelector(".size-x").addEventListener("input", function () {
+   vertCount[0] = parseInt(this.value);
+   c.clearRect(0, 0, canvasSize[0], canvasSize[1]);
+   verts = [];
+   createGradient();
+   generateVertices();
+});
+
+document.querySelector(".size-y").addEventListener("input", function () {
+   vertCount[1] = parseInt(this.value);
+   c.clearRect(0, 0, canvasSize[0], canvasSize[1]);
+   verts = [];
+   createGradient();
+   generateVertices();
+});
+
+document.querySelector(".snap-diagonal").addEventListener("click", function () {
+   if (arrowSuffix == "s") {
+      arrowSuffix = "d";
+      arrowOffset = -45;
+      document.querySelector(".snap-diagonal").classList.add("btn-selected");
+      document.querySelector(".snap-straight").classList.remove("btn-selected");
+      for (let a = 0; a < 4; a++) {
+         document.querySelector(".arrow-" + (a + 1) + "-s").classList.add("arrow-" + (a + 1) + "-d");
+         document.querySelector(".arrow-" + (a + 1) + "-d").classList.remove("arrow-" + (a + 1) + "-s");
+      }
+   }
+});
+
+document.querySelector(".snap-straight").addEventListener("click", function () {
+   if (arrowSuffix == "d") {
+      arrowSuffix = "s";
+      arrowOffset = 0;
+      document.querySelector(".snap-straight").classList.add("btn-selected");
+      document.querySelector(".snap-diagonal").classList.remove("btn-selected");
+      for (let a = 0; a < 4; a++) {
+         document.querySelector(".arrow-" + (a + 1) + "-d").classList.add("arrow-" + (a + 1) + "-s");
+         document.querySelector(".arrow-" + (a + 1) + "-s").classList.remove("arrow-" + (a + 1) + "-d");
+      }
+   }
+});
+
+document.querySelector(".arrow-1").addEventListener("click", function () {
+   gradAngle = arrowAngles[arrowSuffix][0];
+   document.querySelector(".rotation").value = gradAngle;
+   createGradient();
+   drawBoxes();
+});
+document.querySelector(".arrow-2").addEventListener("click", function () {
+   gradAngle = arrowAngles[arrowSuffix][1];
+   document.querySelector(".rotation").value = gradAngle;
+   createGradient();
+   drawBoxes();
+});
+document.querySelector(".arrow-3").addEventListener("click", function () {
+   gradAngle = arrowAngles[arrowSuffix][2];
+   document.querySelector(".rotation").value = gradAngle;
+   createGradient();
+   drawBoxes();
+});
+document.querySelector(".arrow-4").addEventListener("click", function () {
+   gradAngle = arrowAngles[arrowSuffix][3];
+   document.querySelector(".rotation").value = gradAngle;
+   createGradient();
+   drawBoxes();
+});
 
 
 /*
@@ -63,28 +183,22 @@ MODAL
 ===================================================================================
 */
 
-document.querySelector(".palette").addEventListener("click", function () {
-	document.querySelector(".color-modal").style.opacity = "1";
-	document.querySelector(".color-modal").style.visibility = "visible";
-	document.querySelector(".overlay").style.opacity = "1";
-	document.querySelector(".overlay").style.visibility = "visible";
-	tempColors = colors.concat();
-	showColorPalette(true);
-});
-
 document.querySelector(".modal-confirm").addEventListener("click", function () {
-	document.querySelector(".color-modal").style.opacity = "0";
+   document.querySelector(".color-modal").style.opacity = "0";
 	document.querySelector(".color-modal").style.visibility = "hidden";
 	document.querySelector(".overlay").style.opacity = "0";
 	document.querySelector(".overlay").style.visibility = "hidden";
 	colors = [];
 	colors = tempColors.concat();
 	tempColors = [];
-	while (document.querySelector(".palette").firstChild) {
-		document.querySelector(".palette").removeChild(document.querySelector(".palette").firstChild);
-	}
-	showColorPalette();
-	createGradient();
+	// while (document.querySelector(".palette").firstChild) {
+	// 	document.querySelector(".palette").removeChild(document.querySelector(".palette").firstChild);
+   // }
+   c.clearRect(0, 0, canvasSize[0], canvasSize[1]);
+   verts = [];
+	updatePalette(editingPalette);
+   createGradient();
+   generateVertices();
 });
 
 document.querySelector(".modal-cancel").addEventListener("click", function () {
@@ -101,7 +215,7 @@ document.querySelector(".modal-subtract").addEventListener("click", function () 
 		while (document.querySelector(".colors-wrap").firstChild) {
 			document.querySelector(".colors-wrap").removeChild(document.querySelector(".colors-wrap").firstChild);
 		}
-		showColorPalette(true);
+		updateModalColors(true);
 	}
 });
 
@@ -110,10 +224,16 @@ document.querySelector(".modal-add").addEventListener("click", function () {
 	while (document.querySelector(".colors-wrap").firstChild) {
 		document.querySelector(".colors-wrap").removeChild(document.querySelector(".colors-wrap").firstChild);
 	}
-	showColorPalette(true);
+	updateModalColors(true);
 });
 
-
+document.querySelector(".save-new").addEventListener("click", function () {
+   document.querySelector(".color-modal").style.opacity = "0";
+	document.querySelector(".color-modal").style.visibility = "hidden";
+	document.querySelector(".overlay").style.opacity = "0";
+	document.querySelector(".overlay").style.visibility = "hidden";
+   createNewPalette();
+});
 
 
 
@@ -129,7 +249,6 @@ FUNCTIONS
 */
 
 function generateVertices() {
-	// vertCount[0] += 2;
 	let xSpace = canvasSize[0] / (vertCount[0] - 1);
 	let ySpace = canvasSize[1] / (vertCount[1] - 1);
 	for (let a = 0; a < vertCount[0]; a++) {
@@ -166,7 +285,6 @@ function generateVertices() {
 		}
 		verts.push(col);
 	}
-	// console.log(verts);
 	drawBoxes();
 }
 
@@ -177,8 +295,6 @@ function drawBoxes() {
 		for (let b = 0; b < vertCount[1] - 1; b++) {
 			let dir = Math.floor(Math.random() * 2);
 			if (dir == 0) {
-				// \
-
 				avgX1 = (verts[a][b][0] + verts[a + 1][b][0] + verts[a + 1][b + 1][0]) / 3;
             avgY1 = (verts[a][b][1] + verts[a + 1][b][1] + verts[a + 1][b + 1][1]) / 3;
             c.fillStyle = getPixelColor(avgX1, avgY1);
@@ -221,7 +337,6 @@ function drawBoxes() {
                c.stroke();
             }
 			} else {
-
 				avgX1 = (verts[a][b][0] + verts[a + 1][b][0] + verts[a][b + 1][0]) / 3;
 				avgY1 = (verts[a][b][1] + verts[a + 1][b][1] + verts[a][b + 1][1]) / 3;
             c.fillStyle = getPixelColor(avgX1, avgY1);
@@ -268,18 +383,125 @@ function drawBoxes() {
 	}
 }
 
-function showColorPalette(refreshEdit = false) {
-	for (let a = 0; a < colors.length; a++) {
-		if (!refreshEdit) {
-			let newColor = document.createElement("DIV");
+function createNewPalette() {
+   palettes.push(tempColors);
+   tempColors = [];
+   displayPalettes();
+}
+
+function updatePalette(id) {
+   palettes[id] = [];
+   palettes[id] = colors.concat();
+   console.log(palettes);
+   while (document.querySelector(".palette-"+id).querySelector(".p-color")) {
+		document.querySelector(".palette-"+id).removeChild(document.querySelector(".palette-"+id).querySelector(".p-color"));
+   }
+   for (let a = 0; a < palettes[id].length; a++) {
+      let newColor = document.createElement("div");
+		newColor.classList.add("p-color");
+		newColor.style.background = palettes[id][a];
+		document.querySelector(".palette-"+id).appendChild(newColor);
+   }
+}
+
+function displayPalettes() {
+   while (document.querySelector(".palette")) {
+		document.querySelector(".palette-input").removeChild(document.querySelector(".palette"));
+   }
+   for (let a = 0; a < palettes.length; a++) {
+      let phtml = `<div class="palette-options">
+                     <div class="p-o-1 palette-use palette-use-${a}">
+                        <svg viewBox="0 0 24 24">
+                           <path fill="currentColor" d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z" />
+                        </svg>
+                     </div>
+                     <div class="p-o-2 palette-edit palette-edit-${a}">
+                        <svg viewBox="0 0 24 24">
+                           <path
+                              fill="currentColor"
+                              d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z"
+                           />
+                        </svg>
+                     </div>
+                     <div class="p-o-3 palette-delete palette-delete-${a}">
+                        <svg viewBox="0 0 24 24">
+                           <path fill="currentColor" d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
+                        </svg>
+                     </div>
+                  </div>`;
+      let newP = document.createElement("div");
+      newP.classList.add("palette", `palette-${a}`);
+      newP.innerHTML = phtml;
+      document.querySelector(".palette-input").appendChild(newP);
+      document.querySelector(".palette-edit-"+a).addEventListener("click", function() {
+         document.querySelector(".color-modal").style.opacity = "1";
+         document.querySelector(".color-modal").style.visibility = "visible";
+         document.querySelector(".overlay").style.opacity = "1";
+         document.querySelector(".overlay").style.visibility = "visible";
+         tempColors = palettes[a].concat();
+         let savedId = parseInt(this.classList[2].split("-")[2]);
+         editingPalette = savedId;
+         updateModalColors(savedId);
+      });
+      document.querySelector(".palette-use-"+a).addEventListener("click", function() {
+         let savedId = parseInt(this.classList[2].split("-")[2]);
+         activePalette = a;
+         c.clearRect(0, 0, canvasSize[0], canvasSize[1]);
+         verts = [];
+         colors = palettes[savedId];
+         if (document.querySelector(".palette-active")) {
+            document.querySelector(".palette-active").classList.remove("palette-active");
+         }
+         if (!this.parentElement.parentElement.classList.contains("palette-active")) {
+            this.parentElement.parentElement.classList.add("palette-active");
+         }
+         createGradient();
+         generateVertices();
+      });
+      document.querySelector(".palette-delete-" + a).addEventListener("click", function () {
+         palettes.splice(a, 1);
+         if (palettes[a - 1]) {
+            c.clearRect(0, 0, canvasSize[0], canvasSize[1]);
+            verts = [];
+            colors = palettes[a-1];
+            if (document.querySelector(".palette-active")) {
+               document.querySelector(".palette-active").classList.remove("palette-active");
+            }
+            if (!document.querySelector(".palette-"+(a-1)).classList.contains("palette-active")) {
+               document.querySelector(".palette-"+(a-1)).classList.add("palette-active");
+            }
+            createGradient();
+            generateVertices();
+         } else if (palettes[a]) {
+            c.clearRect(0, 0, canvasSize[0], canvasSize[1]);
+            verts = [];
+            colors = palettes[a];
+            if (document.querySelector(".palette-active")) {
+               document.querySelector(".palette-active").classList.remove("palette-active");
+            }
+            if (!document.querySelector(".palette-"+(a+1)).classList.contains("palette-active")) {
+               document.querySelector(".palette-"+(a+1)).classList.add("palette-active");
+            }
+            createGradient();
+            generateVertices();
+         }
+         this.parentElement.parentElement.remove();
+      });
+      for (let b = 0; b < palettes[a].length; b++) {
+         let newColor = document.createElement("div");
 			newColor.classList.add("p-color");
-			newColor.style.background = colors[a];
-			document.querySelector(".palette").appendChild(newColor);
-		}
-	}
+			newColor.style.background = palettes[a][b];
+			newP.appendChild(newColor);
+      }
+   }
+}
+
+function updateModalColors() {
+   // reset modal
 	while (document.querySelector(".colors-wrap").firstChild) {
 		document.querySelector(".colors-wrap").removeChild(document.querySelector(".colors-wrap").firstChild);
-	}
+   }
+   // add all colors to modal
 	for (let b = 0; b < tempColors.length; b++) {
 		let newEditColor = document.createElement("div");
 		newEditColor.classList.add("edit-color");
@@ -306,6 +528,7 @@ function showColorPalette(refreshEdit = false) {
 }
 
 function createGradient() {
+   colors = palettes[activePalette];
 	let grad, x1, y1, x2, y2, xCenterOffset, yCenterOffset;
 	if (radialGradient) {
 	} else {
@@ -371,11 +594,32 @@ function createGradient() {
 function getPixelColor(x, y) {
    x = Math.round(x);
    y = Math.round(y);
-	let r = imageData.data[y * (imageData.width * 4) + x * 4];
-	let g = imageData.data[y * (imageData.width * 4) + x * 4 + 1];
-	let b = imageData.data[y * (imageData.width * 4) + x * 4 + 2];
+   if (x < 0) {
+      x = 0;
+   }
+   if (y < 0) {
+      y = 0;
+   }
+   if (x > canvasSize[0] - 1) {
+      x = canvasSize[0] - 1;
+   }
+   if (y > canvasSize[1] - 1) {
+      y = canvasSize[1] - 1;
+   }
+   let rrand = Math.floor(Math.random() * randColor) * (Math.floor(Math.random() * 2) * -1);
+   let grand = Math.floor(Math.random() * randColor) * (Math.floor(Math.random() * 2) * -1);
+   let brand = Math.floor(Math.random() * randColor) * (Math.floor(Math.random() * 2) * -1);
+   let bb = Math.floor(Math.random() * brightnessBalance);
+	let r = imageData.data[y * (imageData.width * 4) + x * 4] + rrand + bb;
+	let g = imageData.data[y * (imageData.width * 4) + x * 4 + 1] + grand + bb;
+	let b = imageData.data[y * (imageData.width * 4) + x * 4 + 2] + brand + bb;
    let a = imageData.data[y * (imageData.width * 4) + x * 4 + 3];
    return "rgba(" + r + "," + g + "," + b + "," + a + ")";
+}
+
+function adjustAngle() {
+   let diag = radToDeg(Math.atan(canvasSize[1] / canvasSize[0]));
+   arrowAngles.d.push(diag, 180 - diag, diag + 180, 360 - diag);
 }
 
 function setCanvas() {
@@ -392,6 +636,8 @@ function radToDeg(rad) {
 
 
 setCanvas();
-showColorPalette();
+// updateModalColors();
+displayPalettes();
 createGradient();
 generateVertices();
+adjustAngle();
